638e6081e2f0d03688cc76d2c8654e16c5d97bc0
Prince Gbezeh <gbezehp@gmail.com>
2025-05-21 22:21:01 +0000

    config: create project scaffold with code quality tools

---
+++ server/eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+]);
